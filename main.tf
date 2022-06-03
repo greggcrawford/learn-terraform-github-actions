1bd7389dd084b37cdc926e89edbb8f8590e6dc22
Gregg Crawford <gcra@softchoice.com>
2022-06-03 16:07:52 -0400

    update main.tf r1

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "terraformAzure"
+    organization = "terraformAzure1"
 
     workspaces {
       name = "demo-github-actions"
